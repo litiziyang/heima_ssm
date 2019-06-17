6bbcedf6e58bfcc778a47479023929f95b9152ac
lizi <2567707642@qq.com>
2019-06-17 11:33:59 +0800

    垃圾东西已经删除

--- heima_ssm_web/src/main/java/com/itheima/ssm/controller/spring.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.ssm.controller;
-
-public class spring {
-    public static void main(String[] args) {
-        System.out.println("lzy");
-    }
-}
--- heima_ssm_web/src/main/java/com/itheima/ssm/controller/test.java
+++
@@ -1,8 +0,0 @@
-package com.itheima.ssm.controller;
-
-public class test {
-
-    public static void main(String[] args) {
-        System.out.println("你是傻逼");
-    }
-}
